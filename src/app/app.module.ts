fac37a04671fd44539b7561253b0ce21d2debf5e
alexandratarod <alexandratarod@yahoo.ro>
2023-10-14 11:15:56 +0300

    some html and css code to show how the app will look

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CategoryComponent } from './category/category.component';
+import { HomeSectionComponent } from './home-section/home-section.component';
+import { ProductSectionComponent } from './product-section/product-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategoryComponent,
+    HomeSectionComponent,
+    ProductSectionComponent
   ],
   imports: [
     BrowserModule
